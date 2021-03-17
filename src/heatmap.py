ad4d501dde9d99fc2d10f0827d1bd341f1dcf47e
Albert ten Oever <agtoever@hotmail.com>
2021-03-17 16:59:09 +0100

    Fixed some heatmap bugs

--- src/heatmap.py
+++ src/heatmap.py
@@ -7,25 +7,28 @@ class Heatmap:
     def __init__(self, game, bot):
         self.game = game
         self.bot = bot
+        self.policy_moves = None
         self.scores = numpy.full((self.game.SIZE, self.game.SIZE), 
                                  numpy.nan,
                                  numpy.single)
         self.calculate()
 
 
-    def _get_score(self):
-        sc, _ = self.bot.nm.eval_game(self.game)
-        return sc
-
-
     def calculate(self):
-        base_sc = self._get_score()
+        # Get current win probability as base score
+        base_sc, self.policy_moves = self.bot.nm.eval_game(self.game)
+        
+        # Unpack policy moves from (move, P)-list
+        self.policy_moves = [str(m) for m, p in self.policy_moves]
+        
+        # Loop over all legal moves and calculate and store score in heatmap
         for move in self.game.slow_legal_plays():
             if move != twixt.SWAP:
                 self.game.play(move)
-                self.scores[move] = base_sc - self._get_score()
+                sc, moves = self.bot.nm.eval_game(self.game)
+                self.scores[move] = base_sc - sc
                 self.game.undo()
             
-        # normalize
+        # normalize heatmap
         scalar = max(-numpy.nanmin(self.scores), numpy.nanmax(self.scores))
         self.scores /= scalar
