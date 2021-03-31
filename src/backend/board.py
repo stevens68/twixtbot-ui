66720977af258a1124b0caf673ec1631ef805505
Albert ten Oever <agtoever@hotmail.com>
2021-03-31 20:37:24 +0200

    Show outline around last played peg #48

--- src/backend/board.py
+++ src/backend/board.py
@@ -157,14 +157,17 @@ class TwixtBoard:
         nho.objects = objs
         # end set_nn_inputs
 
-    def _create_drawn_peg(self, point, coloridx):
+    def _create_drawn_peg(self, point, coloridx, outline_move=False):
         if coloridx == 1:
-            color = self.stgs.get(ct.K_COLOR[1])
+            color = lcolor = self.stgs.get(ct.K_COLOR[1])
         else:
-            color = self.stgs.get(ct.K_COLOR[2])
+            color = lcolor = self.stgs.get(ct.K_COLOR[2])
+        
+        if outline_move:
+            lcolor = ct.SHOW_LAST_MOVE[1]
 
         peg = self.graph.DrawCircle(self._point_to_coords(
-            point), self.peg_radius, color, color)
+            point), self.peg_radius, color, lcolor, ct.SHOW_LAST_MOVE[2])
         return peg
 
     def create_move_objects(self, game, index):
@@ -183,7 +186,8 @@ class TwixtBoard:
         nho = TBWHistory(move)
         self.history.append(nho)
 
-        nho.objects.append(self._create_drawn_peg(move, color))
+        outline_move = ct.SHOW_LAST_MOVE[0] and index == len(game.history) - 1
+        nho.objects.append(self._create_drawn_peg(move, color, outline_move))
         self.known_moves.add(move)
 
         for dlink in game.DLINKS:
